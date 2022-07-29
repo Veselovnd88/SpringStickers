ca5b1aca8061f86471b2984d1cada062159c3d30
Nikolay <veselovnd@gmail.com>
2022-07-29 15:46:51 +0300

    валидация

--- src/main/java/ru/veselov/springstickers/controllers/SpringLabelController.java
+++ src/main/java/ru/veselov/springstickers/controllers/SpringLabelController.java
@@ -1,6 +1,7 @@
 package ru.veselov.springstickers.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
