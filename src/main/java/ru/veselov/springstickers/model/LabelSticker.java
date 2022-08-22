a101603f1821798edd6e13fc4c3de8d8a44ab8dc
Veselov Nikolay <veselovnd@gmail.com>
2022-08-22 15:40:13 +0300

    fix: check font

--- src/main/java/ru/veselov/springstickers/model/LabelSticker.java
+++ src/main/java/ru/veselov/springstickers/model/LabelSticker.java
@@ -55,8 +55,8 @@ public class LabelSticker extends AbstractLabel{
 
 	@Override
 	public Image createImage() {
-	      Font stringFont = new Font("Courier New",Font.BOLD,13);//задаем стандартный шрифт
-	      Font stringBold = new Font("Courier New",Font.BOLD,15);//задаем жирный шрифт
+	      Font stringFont = new Font("TrueType",Font.BOLD,13);//задаем стандартный шрифт
+	      Font stringBold = new Font("TrueType",Font.BOLD,15);//задаем жирный шрифт
 	      this.g.setFont(stringFont);//установка обычного шрифта
 	      this.g.setColor(Color.black);//установка цвета шрифта
 	      this.g.drawString(range, 80,51);//рисуем диапазон
