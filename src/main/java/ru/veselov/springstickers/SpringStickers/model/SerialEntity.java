861941ecba7efea3f15ba162241f693541901263
Veselov Nikolay <veselovnd@gmail.com>
2022-10-28 13:06:02 +0300

    add: serial saving function

--- src/main/java/ru/veselov/springstickers/SpringStickers/model/SerialEntity.java
+++ src/main/java/ru/veselov/springstickers/SpringStickers/model/SerialEntity.java
@@ -1,16 +1,18 @@
 package ru.veselov.springstickers.SpringStickers.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.Date;
 
 @Entity
+@Data
 @Table(name = "serial_num")
 public class SerialEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private int serial_id;
 
     @Column(name = "num")
     private String num;
@@ -21,37 +23,5 @@ public class SerialEntity {
     @ManyToOne
     @JoinColumn(name="id", nullable = false)
     private LabelEntity label;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNum() {
-        return num;
-    }
-
-    public void setNum(String num) {
-        this.num = num;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LabelEntity getLabel() {
-        return label;
-    }
-
-    public void setLabel(LabelEntity label) {
-        this.label = label;
-    }
 
 }
